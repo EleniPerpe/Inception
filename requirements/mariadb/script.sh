c2f018ddd9975643475d9428feae1d154a18827d
Eleni Perperidou <eperperi@1-D-19.42heilbronn.de>
2025-04-02 18:55:17 +0200

    .env and makefile ready

--- requirements/mariadb/script.sh
+++ requirements/mariadb/script.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
+# Initialize the MariaDB data directory
 mysql_install_db
-mysqld
+
+# Start the MariaDB server in the background
+mysqld --skip-networking &
+mariadb_pid=$!
+
+# Wait for MariaDB to be ready
+until mysqladmin ping --silent; do
+    echo "Waiting for MariaDB to be ready..."
+    sleep 2
+done
+
+# Use environment variables to create the database, user, and grant privileges
+mysql -u root <<-EOSQL
+    CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+    GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+    FLUSH PRIVILEGES;
+EOSQL
+
+# Stop the MariaDB server
+kill "$mariadb_pid"
+wait "$mariadb_pid"
+
+# Restart MariaDB in the foreground
+exec mysqld
--- requirements/wordpress/script.sh
+++ requirements/wordpress/script.sh
@@ -1,9 +1,48 @@
 #!/bin/bash
+
 cd /var/www/html
+
+# Download WP-CLI
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
 
+# Wait for MariaDB to be ready
+# until mysqladmin ping -h"$WORDPRESS_DB_HOST" --silent; do
+#     echo "Waiting for MariaDB to be ready..."
+#     sleep 5
+# done
+
+# Download and configure WordPress
+./wp-cli.phar core download --allow-root
+./wp-cli.phar config create \
+    --dbname="$WORDPRESS_DB_NAME" \
+    --dbuser="$WORDPRESS_DB_USER" \
+    --dbpass="$WORDPRESS_DB_PASSWORD" \
+    --dbhost="$WORDPRESS_DB_HOST" \
+    --allow-root
+./wp-cli.phar core install \
+    --url="$WP_URL" \
+    --title="$WP_TITLE" \
+    --admin_user="$WP_ADMIN_USER" \
+    --admin_password="$WP_ADMIN_PASSWORD" \
+    --admin_email="$WP_ADMIN_EMAIL" \
+    --allow-root
+
+
+# Add a sample post
+./wp-cli.phar post create \
+    --post_title="Sample Post" \
+    --post_content="This is a sample post created during initialization." \
+    --post_status=publish \
+    --post_author=1 \
+    --allow-root
+
+# Add a regular WordPress user
+./wp-cli.phar user create \
+    "$WP_USER" "$WP_USER_EMAIL" \
+    --role=editor \
+    --user_pass="$WP_PASS" \
+    --allow-root
+
+# Start PHP-FPM
 php-fpm8.2 -F
